7f92bf12977cbbde2e09e3893bae10fd95103ccc
Lyn Ho <lyn4develop@gmail.com>
2020-04-09 19:45:26 +0800

    打包 html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
+
   module: {
     rules: [
       {
@@ -8,5 +11,18 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+
+  plugins: [
+    // 数组，存放所有 webpack 插件
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',  // 打包后的文件名
+      minify: {
+        removeAttributeQuotes: false,  // 是否删除属性的双引号
+        collapseWhitespace: false  // 是否折叠空白
+      },
+      // hash: true  // 是否加上 hash，默认是 false
+    })
+  ]
 }
