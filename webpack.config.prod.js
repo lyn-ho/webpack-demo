92522ca61b476afe7d4e17cad54824f2992c4726
Lyn Ho <lyn4develop@gmail.com>
2020-04-10 16:07:31 +0800

    BundleAnalyzerPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,7 @@ const base = require('./webpack.config.base')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const smp = new SpeedMeasurePlugin()
 
@@ -21,7 +22,9 @@ const config = smart(base, {
 
     new webpack.DefinePlugin({
       DEV: JSON.stringify('production')
-    })
+    }),
+
+    new BundleAnalyzerPlugin,
   ]
 })
 
